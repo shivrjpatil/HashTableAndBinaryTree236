4e0d3d31674719491a6e87a7e9bd3330d97db7c2
shivrjpatil <shivrajp972@gmail.com>
2023-01-13 19:29:40 +0530

    Added a program to find frequency of words in hash table

---
+++ HashTableBinaryTreee/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableBinaryTreee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data Structure Problems Using C-Sharp Generics");
+            string paragraph = "To be or not to be";
+            CountNumbOfOccurence(paragraph);
+            Console.ReadLine();
+
+        }
+        public static void CountNumbOfOccurence(string paragraph)
+        {
+            MyMapNode<string, int> hashTabe = new MyMapNode<string, int>(6);
+
+            string[] words = paragraph.Split(' ');
+
+            foreach (string word in words)
+            {
+                if (hashTabe.Exists(word.ToLower()))
+                    hashTabe.Add(word.ToLower(), hashTabe.Get(word.ToLower()) + 1);
+                else
+                    hashTabe.Add(word.ToLower(), 1); 
+            }
+            hashTabe.Display();
+
+        }
+    }
+}
+
